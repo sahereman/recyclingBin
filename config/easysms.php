036038a1bd0b9e762b02c6c9be6eacaeda5f791e
Miracle <120007700@qq.com>
2019-08-28 10:16:11 +0800

    install easy sms

---
+++ config/easysms.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    // HTTP 请求的超时时间（秒）
+    'timeout' => 5.0,
+
+    // 默认发送配置
+    'default' => [
+        // 网关调用策略，默认：顺序调用
+        'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+        // 默认可用的发送网关
+        'gateways' => [
+            'aliyun',
+        ],
+    ],
+    // 可用的网关配置
+    'gateways' => [
+        'errorlog' => [
+            'file' => '/tmp/easy-sms.log',
+        ],
+        'aliyun' => [
+            'access_key_id' => env('SMS_ALIYUN_KEY'),
+            'access_key_secret' => env('SMS_ALIYUN_SECRET'),
+            'sign_name' => '青岛尚禾维曼网络科技',
+        ]
+    ],
+];
