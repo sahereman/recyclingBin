90b9021785e77dbfb1b2a654f533c7f020f0f68a
Miracle <120007700@qq.com>
2019-10-17 10:54:36 +0800

    回收订单测试数据 2

--- database/seeds/CleanOrdersSeeder.php
+++ database/seeds/CleanOrdersSeeder.php
@@ -16,21 +16,25 @@ class CleanOrdersSeeder extends Seeder
         $recyclers->each(function ($recycler) {
             $bins = $recycler->bins;
 
-            for ($i = 1; $i <= 20; $i++)
+            if($bins)
             {
-                $bin = $bins->random();
-                $order = factory(CleanOrder::class)->create([
-                    'bin_id' => $bin->id,
-                    'recycler_id' => $recycler->id,
-                ]);
+                for ($i = 1; $i <= 20; $i++)
+                {
+                    $bin = $bins->random();
+                    $order = factory(CleanOrder::class)->create([
+                        'bin_id' => $bin->id,
+                        'recycler_id' => $recycler->id,
+                    ]);
 
-                factory(CleanOrderItem::class, random_int(1, 2))->create([
-                    'order_id' => $order->id,
-                ]);
+                    factory(CleanOrderItem::class, random_int(1, 2))->create([
+                        'order_id' => $order->id,
+                    ]);
 
-                GenerateRecycleOrderSnapshot::dispatch($order, $bin);
-                RecyclerMoneyBill::change($recycler, RecyclerMoneyBill::TYPE_RECYCLE_ORDER, $order->total, $order);
+                    GenerateRecycleOrderSnapshot::dispatch($order, $bin);
+                    RecyclerMoneyBill::change($recycler, RecyclerMoneyBill::TYPE_RECYCLE_ORDER, $order->total, $order);
+                }
             }
+
         });
 
     }
