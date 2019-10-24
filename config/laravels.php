6e137796c6b43f0ad67b29900830caa570755cc6
Miracle <120007700@qq.com>
2019-10-24 16:34:44 +0800

    429 调试

--- config/laravels.php
+++ config/laravels.php
@@ -63,13 +63,13 @@ return [
     'events'                   => [],
     'swoole_tables'            => [],
     'register_providers'       => [
-        /* middleware service providers */
-        Dingo\Api\Provider\DingoServiceProvider::class,
-        Dingo\Api\Provider\HttpServiceProvider::class,
-        /* auth service providers */
-        Illuminate\Auth\AuthServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+//        /* middleware service providers */
+//        Dingo\Api\Provider\DingoServiceProvider::class,
+//        Dingo\Api\Provider\HttpServiceProvider::class,
+//        /* auth service providers */
+//        Illuminate\Auth\AuthServiceProvider::class,
+//        App\Providers\AuthServiceProvider::class,
+//        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
     ],
     'cleaners'                 => [
         Hhxsv5\LaravelS\Illuminate\Cleaners\SessionCleaner::class, // If you use the session or authentication in your project, please uncomment this line
