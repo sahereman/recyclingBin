bc4ea631d1cc126e46beffe65b1cb19df2454e5f
Miracle <120007700@qq.com>
2019-11-29 11:32:29 +0800

    生成6000个传统箱

--- database/seeds/BoxesSeeder.php
+++ database/seeds/BoxesSeeder.php
@@ -14,16 +14,15 @@ class BoxesSeeder extends Seeder
     public function run()
     {
         BoxAdminUser::truncate();
-        Box::where('id', '>', 0)->delete();
 
         //回收员
         $box_admin_user = \Encore\Admin\Auth\Database\Role::where('slug', 'box_admin')->first()->administrators->first();
 
         // 青岛站
-        $site = ServiceSite::where('city', '青岛市')->first();
+        $site = ServiceSite::all()->first();
         $lat = '36.092550';
         $lng = '120.381420';
-        for ($i = 1; $i <= 20; $i++)
+        for ($i = 1; $i <= 6000; $i++)
         {
             $box = factory(Box::class)->create([
                 'site_id' => $site->id,
@@ -35,30 +34,6 @@ class BoxesSeeder extends Seeder
 
             // 回收箱绑定回收员
             if ($i <= 3)
-            {
-                BoxAdminUser::create([
-                    'box_id' => $box->id,
-                    'admin_user_id' => $box_admin_user->id,
-                ]);
-            }
-        }
-
-        // 济南站
-        $site = ServiceSite::where('city', '济南市')->first();
-        $lat = '36.660958';
-        $lng = '117.016158';
-        for ($i = 1; $i <= 5; $i++)
-        {
-            $box = factory(Box::class)->create([
-                'site_id' => $site->id,
-                'lat' => $lat,
-                'lng' => $lng,
-            ]);
-            $lat = bcsub($lat, '0.001', 6);
-            $lng = bcsub($lng, '0.001', 6);
-
-            // 回收箱绑定回收员
-            if ($i <= 2)
             {
                 BoxAdminUser::create([
                     'box_id' => $box->id,
