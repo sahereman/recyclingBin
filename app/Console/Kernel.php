e219562fc02fe7a2dd1b781e19fd1112f132b39d
Miracle <120007700@qq.com>
2019-12-20 10:13:50 +0800

    定时备份数据库

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('backup:run --disable-notifications --only-db')->twiceDaily(1,13)->runInBackground(); // 定时备份数据库
     }
 
     /**
