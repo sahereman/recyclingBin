3dea18470e6311d79058615cb68160eb3583ce99
Miracle <120007700@qq.com>
2019-12-23 16:04:25 +0800

    微信充值提醒

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\SendSms;
+use App\Models\RecyclerPayment;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -9,7 +11,6 @@ class Kernel extends ConsoleKernel
 {
     /**
      * The Artisan commands provided by your application.
-     *
      * @var array
      */
     protected $commands = [
@@ -18,25 +19,53 @@ class Kernel extends ConsoleKernel
 
     /**
      * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
-        $schedule->command('backup:run --disable-notifications --only-db')->twiceDaily(1,13)->runInBackground(); // 定时备份数据库
+        $schedule->command('backup:run --disable-notifications --only-db')->twiceDaily(1, 13)->runInBackground(); // 定时备份数据库
+
+        $schedule->call(function () {
+
+            if (app()->environment('production'))
+            {
+                $phones = [
+                    '18600982820',
+                    '18363928677',//李飞
+                    '18561386786',//郎丰江
+                    '15550809002',//李千千
+                    '15154152876',//周金芳
+                    '18366268341',//李俊
+                ];
+                $r_payment = RecyclerPayment::whereBetween('paid_at', [
+                    now()->startOfDay(),// start
+                    now()->endOfDay(),// end
+                ])->get();
+
+                if ($r_payment->isEmpty())
+                {
+                    $date = now()->toDateTimeString();
+                    $content = "今天截止到 {$date},还未进行微信支付充值,收到短信后前往小黑点回收员充值";
+                    foreach ($phones as $v)
+                    {
+                        SendSms::dispatch($v, 'SMS_180961787', $content, [
+                            'date' => $date
+                        ]);
+                    }
+
+                }
+            }
+        })->dailyAt('19:00');
     }
 
     /**
      * Register the commands for the application.
-     *
      * @return void
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
